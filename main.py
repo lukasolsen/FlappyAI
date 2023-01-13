de5eaf6152f284fbc5e0600b4c2f5487429ae1bc
lukasolsen <lukmarwil@gmail.com>
2023-01-13 20:46:01 +0100

    Final Commit

---
+++ main.py
@@ -0,0 +1,342 @@
+import pygame
+import neat
+from neat.genome import DefaultGenome
+import time
+import os
+import random
+import pickle
+import re
+import json
+import matplotlib.pyplot as plt
+pygame.font.init()
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+GEN = 0
+
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))), pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))), pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+GEN_INFO = {}
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
+class Stats:
+    def __init__(self):
+        self.filepath = "stats.json"
+    def show(self):
+        """Setup the graph to show stats. Will show the fitness level over the generations."""
+        
+        import matplotlib.pyplot as plt
+
+        with open(self.filepath, "r") as f:
+            data = json.load(f)
+
+        
+        names = [f"GEN {k}" for k in data.keys()]
+        fitness = [v["fitness"] for v in data.values()]
+
+        
+        fig, ax = plt.subplots()
+
+        ax.plot(names, fitness)
+
+        ax.set_ylabel("Fitness")
+        ax.set_title("Final Results of the AI")
+
+        plt.show()
+
+
+
+
+class Bird:
+    IMGS = BIRD_IMAGES
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
+
+        if d >= 16:
+            d = 16
+        
+        if d <= 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+        
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+        
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+        
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+        
+        return False
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, birds, pipes, base, score, gen):
+    win.blit(BG_IMG, (0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    text = STAT_FONT.render("Gen: " + str(gen), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
+
+    base.draw(win)
+    for bird in birds:
+        bird.draw(win)
+    pygame.display.update()
+
+def write_stats(ge):
+    if os.path.exists("stats.json") and not GEN == 1:
+        with open("stats.json", "r") as f:
+            z = json.load(f)
+
+        info_1 = {
+            "name": GEN,
+            "fitness": ge[0].fitness
+        }
+        z[GEN] = info_1
+
+        with open("stats.json", "w") as f:
+            json.dump(z, f, indent=4)
+    else:
+        info_1 = {
+            GEN: {
+            "name": GEN,
+            "fitness": ge[0].fitness
+            }
+        }
+        with open("stats.json", "w") as f:
+            json.dump(info_1, f, indent=4)
+
+def main(genomes, config):
+    global GEN
+    GEN += 1
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+        
+
+    base = Base(730)
+    pipes = [Pipe(600)]
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    score = 0
+
+    run = True
+    while run:
+        clock.tick(480)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+
+        pipe_ind = 0
+
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
+        
+        add_pipe = False
+        rem = []
+        for pipe in pipes:
+            for x, bird in enumerate(birds):
+
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    if len(birds) == 1:
+                        write_stats(ge)
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+            
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            pipe.move()
+        if add_pipe:
+            score += 1
+            for g in ge:
+                g.fitness += 5
+            pipes.append(Pipe(600))
+        
+        for r in rem:
+            pipes.remove(r)
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+                
+
+        base.move()
+        draw_window(win, birds, pipes, base, score, GEN)
+
+        if score > 10:
+            write_stats(ge)
+            pickle.dump(nets[0],open("best.pickle", 'wb'))
+            pickle.dump(ge[0], open("besta.pickle", 'wb'))
+            break
+
+def run(config_path):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 10)
+    print('\nBest genome:\n{!s}'.format(winner))
+    Stats().show()
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+   
+    
+    run(config_path)
---
+++ readme.md
@@ -0,0 +1,96 @@
+
+#  FlappyAI
+
+  
+
+![FlappyAI](https://imageio.forbes.com/specials-images/imageserve/621edb0064db88ccfcec6462/0x0.jpg?format=jpg&crop=4823,3238,x735,y238,safe&width=800)
+
+  
+
+FlappyAI is a modified and edited version of the original Flappy Bird AI created by Tech With Tim. We have implemented additional features and functionality to enhance the overall gameplay experience. The AI utilizes a NeuroEvolution of Augmenting Topologies (NEAT) algorithm to train the agents to play the game.
+
+  
+
+##  Team
+
+- Developer 1
+
+- Developer 2
+
+- Developer 3
+
+  
+
+##  About
+
+FlappyAI is an artificial intelligence (AI) model that has been trained to play the popular mobile game, Flappy Bird. The AI utilizes a NeuroEvolution of Augmenting Topologies (NEAT) algorithm to train the agents to play the game. The NEAT algorithm is a type of evolutionary algorithm that is used to evolve artificial neural networks. In this case, the NEAT algorithm is used to evolve the neural networks of the agents that play the game.
+
+  
+
+The AI has been trained on a dataset of several thousand games, and has been tested on several hundred more. The AI has been able to achieve a high level of performance and is able to successfully play the game at a very high level.
+
+  
+
+##  Installation
+
+  
+
+To use FlappyAI, you will need to have the following modules installed:
+
+- pygame
+
+- neat-python
+
+- graphviz
+
+  
+
+You can install these modules by running the following command:
+
+    pip install pygame neat-python graphviz
+   
+## API 
+We have also implemented a simple API that allows you to interact with the AI and access its functionality. Here is an example of how you can use the API in Python: 
+```
+import Api.flappy_bird as api
+
+# Initialize the API
+
+# generations_amount: number of generations to train the AI for
+
+# stats_path: file path to save the statistics of the AI training
+
+# images_folder: folder path to save the images of the AI playing the game
+
+api_instance = api.Api(generations_amount=10, stats_path="stats", images_folder="imgs")
+
+  
+
+# Run the API
+
+# max_score: the maximum score the AI should try to reach
+
+api_instance.run(max_score=10)
+```
+
+## Warning
+
+Please note that the AI has been trained using the specified modules, if you use any other version of these modules it may not work as intended.
+
+## Acknowledgements
+
+We would like to thank Tech With Tim for providing the original source code for the Flappy Bird AI. You can find Tech With Tim's socials here:
+
+-   Youtube: [Tech With Tim](https://www.youtube.com/c/techwithtim)
+-   Twitter: [@techwithtim](https://twitter.com/techwithtim)
+-   Instagram: [@techwithtim](https://www.instagram.com/techwithtim/)
+-   Website: [techwithtim.net](https://techwithtim.net/)
+
+We would also like to thank the creators of the NEAT algorithm, Kenneth O. Stanley and Risto Miikkulainen for their work on the algorithm. You can find a guide to the NEAT algorithm here: [A guide to the NEAT Algorithm](https://www.techwithtim.net/tutorials/neat-algorithm/)
+
+## Contribution
+
+If you would like to contribute to the project, please feel free to fork the repository and make a pull request with your changes.
+
+## License
+The MIT License (MIT) Copyright (c) [2023] [Lukas Olsen] Permission is hereby granted, free  of charge, to  any person obtaining a copy  of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
